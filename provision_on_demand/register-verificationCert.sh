7ee6d94b23c07ee9a16a309d8109ebd99cf5b26e
Richard Barnes-Webb <Richard.Barnes-Webb@paconsulting.com>
2021-04-23 10:38:48 +0100

    _______________ < Popping stash >  ---------------        \   \_______  v__v   \  \   O   )  (**)      ||----w |  (__)      ||     ||  \/\   U

--- provision_on_demand/register-verificationCert.sh
+++ provision_on_demand/register-verificationCert.sh
@@ -7,14 +7,15 @@
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 BASE_DIR=`cd "${0%/*}/." && pwd`
 
-
-export OPENSSL_CONF=${BASE_DIR}/CA.conf
-CA_DATA_DIR=${BASE_DIR}/ca_data
 CERTS="$BASE_DIR/certs" && [ ! -d "$CERTS" ] && mkdir $CERTS
-SUBJ="/C=UK/ST=London/L=London/O=barnes-webb.com/CN="
 
+ROOT_KEY=$CERTS/rootCA.private.key
+ROOT_CERT=$CERTS/rootCA.pem.cert
 
-[ ! -f "$OPENSSL_CONF" ] && echo "ERROR: Expecting CA conf file at: $OPENSSL_CONF"  && exit 5
+VERFIC_KEY=$CERTS/verificationCert.private.key
+VERFIC_CSR=$CERTS/verificationCert.csr
+VERFIC_CERT=$CERTS/verificationCert.signed.cert
+
 [ ! -f "${BASE_DIR}/AWS_IOT_ENVARS" ] && echo "ERROR: Expecting AWS_IOT_ENVARS file"  && exit 18
 
 source AWS_IOT_ENVARS
@@ -31,27 +32,24 @@ AWS_REG_CODE="$(aws iot get-registration-code | jq -r '.registrationCode')"
 echo "Code is: ${WHITE}$AWS_REG_CODE${OFF}"
 
 
-head "2) Creating Verification Cert key"
-openssl genrsa                                      \
-    -out $CERTS/verificationCert.private.key        \
-
-head "3) Creating CSR for Verification Cert key"
-openssl req                                         \
-    -new                                            \
-    -extensions v3_req                              \
-    -key ${CERTS}/verificationCert.private.key      \
-    -out ${CERTS}/verificationCert.csr              \
-    -subj "${SUBJ}${AWS_REG_CODE}"                  \
+head "2) Creating CSR for Root key"
+openssl req                     \
+    -new                        \
+    -key $ROOT_KEY              \
+    -out $VERFIC_CSR            \
+    -subj "/CN=${AWS_REG_CODE}" \
 
 
-head "4) Signing verificationCert key"
-openssl ca -batch  \
-    -keyfile ${CA_DATA_DIR}/rootCA.private.key      \
-    -in      ${CERTS}/verificationCert.csr          \
-    -out     ${CERTS}/verificationCert.signed.cert  \
+head "3) Signing verificationCert key"
+openssl x509 -req -sha256       \
+    -days 500  -CAcreateserial  \
+    -in    $VERFIC_CSR          \
+    -CA    $ROOT_CERT           \
+    -CAkey $ROOT_KEY            \
+    -out   $VERFIC_CERT         \
 
 
-head "5) Register verificationCert and registration-config template in AWS"
+head "4) Register verificationCert and registration-config template in AWS"
 echo "JITP_ROLEARN: $JITP_ROLEARN"
 REGISTER_CONFIG_TEMPL=$BASE_DIR/provisioning-template.json
 REGISTER_DETAILS="$BASE_DIR/AWS_cert_register_details.$(date +%Y-%m-%d_%Hh%M).json"
@@ -61,12 +59,12 @@ cat <<EOT > $REGISTER_CONFIG_TEMPL
  "templateBody": "{ \"Parameters\" : { \"AWS::IoT::Certificate::Country\" : { \"Type\" : \"String\" }, \"AWS::IoT::Certificate::Id\" : { \"Type\" : \"String\" } }, \"Resources\" : { \"thing\" : { \"Type\" : \"AWS::IoT::Thing\", \"Properties\" : { \"ThingName\" : {\"Ref\" : \"AWS::IoT::Certificate::Id\"}, \"AttributePayload\" : { \"version\" : \"v1\", \"country\" : {\"Ref\" : \"AWS::IoT::Certificate::Country\"}} } }, \"certificate\" : { \"Type\" : \"AWS::IoT::Certificate\", \"Properties\" : { \"CertificateId\": {\"Ref\" : \"AWS::IoT::Certificate::Id\"}, \"Status\" : \"ACTIVE\" } }, \"policy\" : {\"Type\" : \"AWS::IoT::Policy\", \"Properties\" : { \"PolicyDocument\" : \"{\\\\\"Version\\\\\": \\\\\"2012-10-17\\\\\",\\\\\"Statement\\\\\": [{\\\\\"Effect\\\\\":\\\\\"Allow\\\\\",\\\\\"Action\\\\\": [\\\\\"iot:Connect\\\\\",\\\\\"iot:Publish\\\\\"],\\\\\"Resource\\\\\" : [\\\\\"*\\\\\"]}]}\" } } } }"
 }
 EOT
-aws iot register-ca-certificate                             \
-    --set-as-active                                         \
-    --allow-auto-registration                               \
-    --ca-certificate      file://${CA_DATA_DIR}/rootCA.cert \
-    --verification-cert   file://${CERTS}/verificationCert.signed.cert \
-    --registration-config file://$REGISTER_CONFIG_TEMPL     \
+aws iot register-ca-certificate                         \
+    --set-as-active                                     \
+    --allow-auto-registration                           \
+    --ca-certificate      file://$ROOT_CERT             \
+    --verification-cert   file://$VERFIC_CERT           \
+    --registration-config file://$REGISTER_CONFIG_TEMPL \
     | jq '. + {"registrationCode":"'$AWS_REG_CODE'"}' | tee $REGISTER_DETAILS
 
 
--- provision_on_demand/setup_CA.sh
+++ provision_on_demand/setup_CA.sh
@@ -7,12 +7,9 @@
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 BASE_DIR=`cd "${0%/*}/." && pwd`
 
-export OPENSSL_CONF=${BASE_DIR}/CA.conf
-CA_DATA_DIR=${BASE_DIR}/ca_data
+CERTS="$BASE_DIR/certs" && [ ! -d "$CERTS" ] && mkdir $CERTS
 SUBJ="/C=UK/ST=London/L=London/O=barnes-webb.com/CN="
 ROOT_CN="Richard Barnes-Webb IoT CA"
-
-[ ! -f "$OPENSSL_CONF" ] && echo "ERROR: Expecting CA conf file at: $OPENSSL_CONF"  && exit 5
 
 head(){
     echo 
@@ -20,25 +17,17 @@ head(){
     echo "= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = ="
     echo "$*"
 }
-
-head "1) Creating $CA_DATA_DIR and empty data files"
-mkdir $CA_DATA_DIR 
-touch $CA_DATA_DIR/index.txt || (echo "ERROR: Unable to create $CA_DATA_DIR.index.txt"  && exit 6)
-echo '01' > $CA_DATA_DIR/serial
+ROOT_KEY=$CERTS/rootCA.private.key
+head "1) Creating rootCA key: $ROOT_KEY"
+openssl genrsa -out $ROOT_KEY    # NB: add `-des3` to genrsa command to have privatekey with password
 
 
-head "2) Creating rootCA key"
-openssl genrsa                              \
-    -out $CA_DATA_DIR/rootCA.private.key    # NB: add `-des3` to genrsa command to have privatekey with password
-
-
-head "3) Creating rootCA cert"
-openssl req -new -x509 -sha256 -days 3650 -nodes -extensions v3_ca   \
-    -key $CA_DATA_DIR/rootCA.private.key            \
-    -out $CA_DATA_DIR/rootCA.cert                   \
+ROOT_CERT=$CERTS/rootCA.pem.cert
+head "2) Creating rootCA cert: $ROOT_CERT"
+openssl req -new -x509 -sha256 -days 3650 -nodes \
+    -key $ROOT_KEY          \
+    -out $ROOT_CERT         \
     -subj "${SUBJ}${ROOT_CN}"
-
-head "$(tree $CA_DATA_DIR)"
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 echo "Done."

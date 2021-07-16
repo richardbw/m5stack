20dca522b15a885606072b582555dfa11586d3ea
Richard Barnes-Webb <Richard.Barnes-Webb@paconsulting.com>
2021-07-16 16:56:55 +0100

    main: I should have had a V8 this morning.

--- provision_on_demand/mqtt_pub.sh
+++ provision_on_demand/mqtt_pub.sh
@@ -1,1 +1,37 @@
-../uiflow/mqtt_pub.sh
+#!/bin/bash
+#
+#  auth: rbw
+#  date: 20210503
+#  desc: 
+#
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+BASE_DIR=`cd "${0%/*}/." && pwd`
+
+MQTT_HOST='a22d4aoxca2zot-ats.iot.us-east-1.amazonaws.com'
+MQTT_TOPIC="rbw/test"
+
+
+CERTS="$BASE_DIR/certs"
+DATE="$(date '+%Y-%m-%d %H:%M:%S')"
+PAYLOAD="{\"timestamp\": \"${DATE}\"}"
+
+echo "Host         : $MQTT_HOST "| sed "s/\(.*\.iot\)\.\(.*\)\.\(amazonaws.com\)/\1.${BROWN}\2${OFF}.\3/"
+echo "Publishing to: ${WHITE}$MQTT_TOPIC${OFF} "
+echo "           at: ${WHITE}${DATE}${OFF}" 
+echo "----------------------"
+mosquitto_pub -q 1 -d                               \
+    --tls-version tlsv1.2 -I "$(basename $0)"       \
+    --cafile    "$CERTS/rootCA.pem.cert"          \
+    --cert      "$CERTS/d_delme1.cert_and_CA-crt.crt"               \
+    --key       "$CERTS/d_delme1.key"               \
+    -h          "$MQTT_HOST" -p 8883                \
+    -t          $MQTT_TOPIC                         \
+    -m          "$PAYLOAD"                          \
+
+if [ "$?" == "0" ] ; then
+    echo "${GREEN}SUCCESS${OFF}"
+else    
+    echo "${RED}ERROR${OFF}: $?"
+fi
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#//EOF
